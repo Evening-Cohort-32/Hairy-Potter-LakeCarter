f027be4620da42a2e9a0e91c3141bb00a28db931
Lake Carter <lakecarter8@gmail.com>
2026-03-10 19:23:20 -0500

    kiln added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Imports go first
 import{makePottery} from './potteryWheel.js'
-
+import { firePottery } from './kiln.js'
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("Mug",3,5)
@@ -9,9 +9,13 @@ let cup = makePottery("cup",1,4)
 let bowl = makePottery("bowl",3,3)
 let pot = makePottery("pot",6,6)
 // Fire each piece of pottery in the kiln
-
-
+mug = firePottery(mug,1500)
+vase = firePottery(vase,2000)
+cup = firePottery(cup,2500)
+bowl = firePottery(bowl,2200)
+pot = firePottery(pot, 2201)
 // Determine which ones should be sold, and their price
 
 
 // Invoke the component function that renders the HTML list
+console.log(cup,vase,mug,bowl,pot)
